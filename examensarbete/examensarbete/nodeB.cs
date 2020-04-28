84bdc6ef31dd405eda840a0fa882d63d213a791d
Nils Pettersson <nils-pettsson@outlook.com>
2020-04-28 19:44:32 +0200

    Created the second Node class made for the reqursiv implementation of a tree structure.

--- examensarbete/examensarbete/nodeB.cs
+++ examensarbete/examensarbete/nodeB.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace examensarbete
 {
-    class NodeB
+    class NodeB:Node
     {
+
+        private NodeB[] children;
+
+        public NodeB()
+        {
+            children = new NodeB[] { };
+        }
+
+        public NodeB[] getChildren()
+        {
+            return children;
+        }
+
+        public void setChildren(NodeB[] children)
+        {
+            this.children = children;
+        }
+
     }
 }
